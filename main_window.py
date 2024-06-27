0d4c48bb1050ae25406cb9d156ec5fbc4f7f1dbc
kartoplyanik <117520721+kartoplyanik@users.noreply.github.com>
2024-06-27 19:29:17 +0300

    Add files via upload

---
+++ main_window.py
@@ -0,0 +1,8 @@
+from PyQt5.QtWidgets import QWidget, QPushButton, QLabel,  QVBoxLayout
+from PyQt5.QtCore import Qt
+
+window = QWIdget()
+window.setWindowTitle("Memory card")
+window.resize(600,500)
+window.move(300,300)
+
